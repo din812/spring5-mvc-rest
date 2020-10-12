81228b14594a34aee66b853144bfa8ba8f65a565
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-10-12 15:45:10 +0300

    poms after refactoring

---
+++ rest-model/src/test/java/din/springframework/restmodel/Spring5MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package din.springframework.restmodel;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5MvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
